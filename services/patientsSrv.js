1e37f540f5136012bc5a6de2b15f29ca6aecf608
bablecode <bable@325south.com>
2022-07-13 09:14:45 -0500

    chg sp_ names

--- services/patientsSrv.js
+++ services/patientsSrv.js
@@ -10,7 +10,7 @@ const config = require('../config/config');
 // };
 
 async function getPatients(recordID){
-  const sql = 'CALL sp_getPatients(?)';
+  const sql = 'CALL sp_PatientsGetAll(?)';
   const [rows, fields] = await db.query(sql,[recordID]);
   return rows[0];
 };
